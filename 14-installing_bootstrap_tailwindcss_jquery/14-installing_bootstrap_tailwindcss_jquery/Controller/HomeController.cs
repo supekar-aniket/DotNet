010bb4a8b703f17b9c314f968beaf64738dd322c
supekar-aniket <supekaraniket34@gmail.com>
2025-02-13 23:17:13 +0530

    How to add or install Bootstrap,Tailwind css,JQuery in our application

---
+++ 14-installing_bootstrap_tailwindcss_jquery/14-installing_bootstrap_tailwindcss_jquery/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _14_installing_bootstrap_tailwindcss_jquery
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
