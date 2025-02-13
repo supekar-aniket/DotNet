cb6c324a60cfdb76b34c77b6949320b8d118cf93
supekar-aniket <supekaraniket34@gmail.com>
2025-02-13 23:13:28 +0530

    How we use Model Classes in our application

---
+++ 10-model_in_mvc/10-model_in_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _10_model_in_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
