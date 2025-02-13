29c33d7c8ed307df96e227d1ae3cbb2f0a74f0ec
supekar-aniket <supekaraniket34@gmail.com>
2025-02-13 23:14:32 +0530

    Why and How we use repository pattern in application

---
+++ 11-repository_pattern/11-repository_pattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _11_repository_pattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 11-repository_pattern/11-repository_pattern/Repository/IStudent.cs
@@ -0,0 +1,11 @@
+﻿using _11_repository_pattern.Models;
+
+namespace _11_repository_pattern.Repository
+{
+    public interface IStudent
+    {
+        List<Student> getAllStudents();
+
+        Student getStudentById(int id);
+    } 
+}
