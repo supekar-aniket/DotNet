4d8c32b16a707f8e3e5538b84272468af11aa771
supekar-aniket <supekaraniket34@gmail.com>
2025-02-18 02:35:26 +0530

    Why we use ViewImport file

---
+++ 15-viewImports_file/15-viewImports_file/Controller/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using _15_viewImports_file.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _15_viewImports_file
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>() {
+                new Student {Id=101,Name="Agastya"},
+                new Student {Id=102,Name="Akash"},
+                new Student {Id=103,Name="Anuja"},
+                new Student {Id=104,Name="Arpita"},
+            };
+
+            return View(students);
+        }
+
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>() {
+                new Student {Id=101,Name="Agastya"},
+                new Student {Id=102,Name="Akash"},
+                new Student {Id=103,Name="Anuja"},
+                new Student {Id=104,Name="Arpita"},
+            };
+
+            return View(students);
+        }
+
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>() {
+                new Student {Id=101,Name="Agastya"},
+                new Student {Id=102,Name="Akash"},
+                new Student {Id=103,Name="Anuja"},
+                new Student {Id=104,Name="Arpita"},
+            };
+
+            return View(students);
+        }
+    }
+}
