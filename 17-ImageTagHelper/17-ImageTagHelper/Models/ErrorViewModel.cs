f19bd9b32efbb09a5d6cd03ce5163b93169f5e8e
supekar-aniket <supekaraniket34@gmail.com>
2025-02-18 02:38:05 +0530

    How and Why we use ImageTagHelper

---
+++ 17-ImageTagHelper/17-ImageTagHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _17_ImageTagHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
