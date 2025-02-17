706e616e8e20600d2007211c999598944d0aa5c8
supekar-aniket <supekaraniket34@gmail.com>
2025-02-18 02:37:00 +0530

    Why we use tagHelpers

---
+++ 16-tagHelpes/16-tagHelpes/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using _16_tagHelpes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _16_tagHelpes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int Contact(int id)
+        {
+            return id;
+        }
+
+        public int Edit(int id)
+        {
+            return id;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
